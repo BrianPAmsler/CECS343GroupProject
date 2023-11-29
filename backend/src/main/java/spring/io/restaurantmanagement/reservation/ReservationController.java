8c64a441ea4864345d1f7bdeb5d81cf7dbee065e
Brian Amsler <manof2words@yahoo.com>
2023-11-28 18:05:37 -0800

    Fixed makeReservation

--- backend/src/main/java/spring/io/restaurantmanagement/reservation/ReservationController.java
+++ backend/src/main/java/spring/io/restaurantmanagement/reservation/ReservationController.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import spring.io.restaurantmanagement.user.UserRepository;
 
 import java.time.LocalDate;
@@ -32,7 +33,19 @@ public class ReservationController {
     }
     
     @PostMapping("/make-reservation")
-    public String makeReservation(Model model, @ModelAttribute Reservation reservation) {
+    public String makeReservation(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String emailAddress, @RequestParam String phoneNumber,
+            @RequestParam String date, @RequestParam String time, @RequestParam Integer guestNumber, @RequestParam String message) {
+        
+        Reservation reservation = new Reservation(firstName,
+                                                  lastName,
+                                                  emailAddress,
+                                                  phoneNumber,
+                                                  LocalDate.parse(date),
+                                                  LocalTime.parse(time),
+                                                  guestNumber,
+                                                  false,
+                                                  message);
+
         reservationRepository.save(reservation);
         return "make-reservation";
     }
