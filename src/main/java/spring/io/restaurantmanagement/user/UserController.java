fe4b10bf3c1ba736b8b0e97a9800f01f499b7e08
Brian Amsler <manof2words@yahoo.com>
2023-11-29 19:07:47 -0800

    Admins can now change roles of users

--- src/main/java/spring/io/restaurantmanagement/user/UserController.java
+++ src/main/java/spring/io/restaurantmanagement/user/UserController.java
@@ -2,10 +2,15 @@ package spring.io.restaurantmanagement.user;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,6 +30,37 @@ public class UserController {
 
         model.addAttribute("usersWrapper", new UserWrapper(userRepository.findAll()));
         model.addAttribute("roles", Role.getRoles());
+        return new ModelAndView("users");
+    }
+    
+    @PostMapping("/users")
+    public ModelAndView postUsers(Model model, @RequestParam Map<String,String> allParams) {
+        // Redirect to home page if user does not have admin role
+        Object userObject = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentUser = userObject instanceof User ? (User) userObject : null;
+        if (currentUser == null || currentUser.getRole() != Role.ROLE_ADMIN) {
+            return new ModelAndView("redirect:/");
+        }
+
+        // Update all changed roles
+        for (var entry : allParams.entrySet()) {
+            if (entry.getKey().startsWith("user-id-")) {
+                int id = Integer.parseInt(entry.getKey().substring(8));
+
+                Optional<User> user = userRepository.findById(id);
+
+                if (user.isPresent()) {
+                    user.get().setRole(Role.valueOf(entry.getValue()));
+
+                    userRepository.save(user.get());
+                }
+            }
+        }
+
+        UserWrapper wrapper = new UserWrapper(userRepository.findAll());
+        model.addAttribute("usersWrapper", wrapper);
+        model.addAttribute("roles", Role.getRoles());
+
         return new ModelAndView("users");
     }
 }
